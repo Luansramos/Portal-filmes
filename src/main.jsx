35cceb3cd614e390ef1009fcc9ed1165a4188c93
luansramos <luanramosgarciadesouza014@gmail.com>
2024-09-13 22:40:47 -0300

    fazendo as coisas

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import MovieListPage from './pages/MovieListPage.jsx'
+import Home from './pages/Home.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+
 
 {/* Deve ter as rotas:
   home
@@ -12,8 +20,26 @@ import './index.css'
   Page Not Found
   */}
 
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <App/>,
+  children :[
+    {index: true, element: <Home/>},
+    {path:'filmes', element:<MovieListPage/>},
+    {path: 'filmes/:id', element: <MovieDetailPage/>},
+    {path: 'generos', element: <GenreListPage/>},
+    {path: 'generos/:id', element: <MoviesByGenrePage/>},
+    {path: "*", element: <PageNotFound/>}
+]}
+
+])
+
+
 createRoot(document.getElementById('root')).render(
+
+
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+export default function PageNotFound(){
+    return(
+        <>
+        <h1>page not found</h1>
+          {/*Lista todos os gêneros disponíveis em um formato de card,
+        cada um com uma cor de fundo diferente, com um link 
+        para uma página que exibe filmes pertencentes a esse gênero. */}
+        </>
+    )
+}
